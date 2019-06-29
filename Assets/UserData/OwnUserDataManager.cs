41dc16ed5eb7a5cef14c6dac06eac003fae8d5ce
Moosan <washer2016@outlook.jp>
2019-06-29 18:14:10 +0900

    OwnDataManager

---
+++ Assets/UserData/OwnUserDataManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+namespace UserData
+{
+    public class OwnUserDataManager : MonoBehaviour
+    {
+        public delegate void UserDataUpdateHandler(UserData userData);
+        public UserDataUpdateHandler OnUpdate;
+        public UserData UserData;
+        private void Start()
+        {
+            UserData = new UserData() {
+                id = "toriaezunoatai",
+                time = DateTime.Now,
+                Lat = 0.000000f,
+                Lon = 0.000000f
+            };
+        }
+        private void OnUserLocationChange()
+        {
+
+        }
+    }
+}
