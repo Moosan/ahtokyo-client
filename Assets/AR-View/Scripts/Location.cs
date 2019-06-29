71d8776fb88e374913e2e7af184fe9b12ecb8cdc
ilim <ilim0t14@gmail.com>
2019-06-29 15:26:34 +0900

    add locate getter

--- Assets/AR-View/Scripts/Location.cs
+++ Assets/AR-View/Scripts/Location.cs
@@ -1,10 +1,29 @@
+using UnityEngine;
+using System;
+
 namespace Scripts
 {
-    public class Location
+    public class Location : MonoBehaviour
     {
-        void AcquisitionLocate()
+        public LocationService locationService = new LocationService();
+
+        private void AcquisitionLocate()
         {
-            
+            if (locationService.isEnabledByUser)
+            {
+                Debug.Log("位置情報が許可されていません");
+            }
+            // TODO: ここで許可を求める
+
+            LocationInfo locationInfo = locationService.lastData; // lastなのでなので本当はstartして待たなければいけない
+
+            Debug.Log(locationInfo.latitude); // 緯度
+            Debug.Log(locationInfo.longitude); // 軽度
+        }
+
+        void Start()
+        {
+            AcquisitionLocate();
         }
     }
 }
