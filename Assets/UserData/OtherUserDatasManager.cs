f9ee7d02a133143033f165a2a4895915a6b79fd9
Moosan <washer2016@outlook.jp>
2019-06-29 18:18:44 +0900

    AddOthers

---
+++ Assets/UserData/OtherUserDatasManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UserData
+{
+    public class OtherUserDatasManager : MonoBehaviour
+    {
+        public delegate void OtherUserDatasUpdateHandler(UserData[] userDataArray);
+        public OtherUserDatasUpdateHandler OnUpdate;
+        public UserData[] OtherUserDatas;
+
+        private void Start()
+        {
+            OtherUserDatas = new UserData[0];
+        }
+    }
+}
