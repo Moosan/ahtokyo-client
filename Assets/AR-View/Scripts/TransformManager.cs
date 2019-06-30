f2b2443ecec71c5d9afb1e447e5352b8e0c21bfd
Hasebe Naoki <naoki0908.11@gmail.com>
2019-06-30 09:48:40 +0900

    色々加えた

--- Assets/AR-View/Scripts/TransformManager.cs
+++ Assets/AR-View/Scripts/TransformManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Scripts
 {
@@ -8,34 +9,52 @@ namespace Scripts
         [SerializeField] private Camera _camera;
         public LocationPoint _LocationPointforother;
         public LocationPoint _LocationPointforown;
-        private Vector3 objposi;
-        public float objhigh;
+        private Vector3 _objposi;
+        private float a;
+        private float b;
+        private Quaternion cam;
 
 
         private void Start()
         {
             _camera = Camera.main;
+            cam = _camera.transform.rotation;
+            Input.location.Start();
             ObjPosition();
             Put();
         }
 
         void ObjPosition()
         {
-            LocationPoint TUS = new LocationPoint(35.69978f, 139.741471f);
+            float degreeAngle = (Vector3.up * -Input.compass.trueHeading).y;
+            LocationPoint TUS = new LocationPoint(35.674788f, 139.768814f);
             LocationPoint KIRARITOGINZA = new LocationPoint(35.674214f, 139.768524f);
             _LocationPointforother = TUS;
             _LocationPointforown = KIRARITOGINZA;
-            var direction = Location.Direction(_LocationPointforown, _LocationPointforother);
-            var distance = Location.Distance(_LocationPointforother, _LocationPointforown);
+            var direction = Location.Direction(_LocationPointforown, _LocationPointforother) / Mathf.PI * 180 - degreeAngle;
+            direction = direction - cam.x;
+            var distance = Location.Distance(_LocationPointforother, _LocationPointforown)/100;
             var rot = Quaternion.AngleAxis(direction, Vector3.up) * _camera.transform.rotation;
+            Debug.Log(rot);
             Vector3 angle = rot.eulerAngles;
-            objposi = new Vector3(angle.x, 0, angle.z).normalized * distance + _camera.transform.position;
-            objposi += Vector3.up * objhigh;
+            transform.eulerAngles = angle;
+            Debug.Log(angle);
+            _objposi = new Vector3(transform.forward.x, 0, transform.forward.z).normalized * distance + _camera.transform.position;
+            _objposi += Vector3.up * (int)Math.Sqrt(direction);
+            Debug.Log(_objposi);
+            Debug.Log(distance);
+            Debug.Log(_camera.transform.rotation);
+            Debug.Log(_camera.transform.position);
         }
 
-        void Put()
+        public void Put()
         {
-            Instantiate(_gameObject, objposi, Quaternion.identity);
+            Instantiate(_gameObject, _objposi, Quaternion.identity);
+            Debug.Log(_objposi);
+        }
+        private void Update()
+        {
+            ObjPosition();
         }
     }
     
